ee7b3c737bc351ed06fa3fbc8584d4f6f427bd40
neelimapriya <neelimasultana6@gmail.com>
2023-10-19 22:28:11 +0600

    setup error page

--- src/Components/Error/ErrorPage.jsx
+++ src/Components/Error/ErrorPage.jsx
@@ -1,9 +1,11 @@
+import Navbar from "../navbar/Navbar";
 
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h2>Error</h2>
+        <div className="">
+            <Navbar></Navbar>
+            <img className="w-full p-52" src="https://i.ibb.co/3kdkknc/errorPge.png" alt="" />
         </div>
     );
 };
